aff4182c46d0369e914cc6740ac2da619648a55b
Vera Nikalaikina <nikolajkina@gmail.com>
2018-10-14 20:06:56 +0300

    Fix scalacheck test

--- build.sbt
+++ build.sbt
@@ -5,4 +5,5 @@ version := "0.1"
 scalaVersion := "2.12.6"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.14.0" % "test"
 libraryDependencies += "org.typelevel" %% "cats-core" % "1.4.0"
--- src/test/scala/io/github/ilyazinovich/dmmf/ValidateOrderProperties.scala
+++ src/test/scala/io/github/ilyazinovich/dmmf/ValidateOrderProperties.scala
@@ -1,64 +1,56 @@
 package io.github.ilyazinovich.dmmf
 
-import cats.data.Validated.Valid
-import io.github.ilyazinovich.dmmf.ProductCode.CheckProductCodeExist
-import io.github.ilyazinovich.dmmf.ValidateOrder.CheckAddressExist
+import cats.data.Validated.{Invalid, Valid}
+import org.scalacheck.Gen
 import org.scalacheck.Prop.forAll
-import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.PropSpec
 import org.scalatest.prop.Checkers
 
 class ValidateOrderProperties extends PropSpec with Checkers {
 
-  private val genAddress: Gen[UnvalidatedAddress] = for {
-    addressLine <- Gen.alphaNumStr
-  } yield UnvalidatedAddress(addressLine)
+  private val genAddress: Gen[UnvalidatedAddress] = Gen.alphaNumStr.map(UnvalidatedAddress)
 
   private val genCustomerInformation: Gen[UnvalidatedCustomerInformation] = for {
-    emailAddress <- Gen.oneOf(Gen.alphaNumStr, Gen.alphaNumStr suchThat (_.contains("@")))
+    emailAddress <- for (a <- Gen.alphaNumStr; b <- Gen.alphaNumStr) yield a + "@" + b
     address <- genAddress
   } yield UnvalidatedCustomerInformation(emailAddress, address)
 
-  private val genWidgetCode: Gen[String] =
-    Gen.alphaNumStr suchThat (code => code.startsWith("W") && code.length == 5)
+  private val genWidget: Gen[(String, Double)] = for {
+    code <- numStrGen(4).map("W" + _)
+    q <- Gen.choose(1, 1000)
+  } yield (code, q)
 
-  private val genGadgetCode: Gen[String] =
-    Gen.alphaNumStr suchThat (code => code.startsWith("G") && code.length == 5)
+  private val genGadget: Gen[(String, Double)] = for {
+    code <- numStrGen(4).map("G" + _)
+    q <- Gen.choose(0.05, 100.0)
+  } yield (code, q)
 
   private val genOrderLine = for {
-    orderLineId <- Gen.alphaNumStr
-    productCode <- Gen.oneOf(Gen.alphaNumStr, genWidgetCode, genGadgetCode)
-    quantity <- Arbitrary.arbitrary[Double]
+    orderLineId <- strGen(50)
+    (productCode, quantity) <- Gen.oneOf(genWidget, genGadget)
   } yield UnvalidatedOrderLine(orderLineId, productCode, quantity)
 
   private val genUnvalidatedOrder: Gen[UnvalidatedOrder] = for {
-    orderId <- Gen.alphaNumStr
+    orderId <- strGen(50)
     customerInformation <- genCustomerInformation
     orderLines <- Gen.listOf(genOrderLine)
   } yield UnvalidatedOrder(orderId, customerInformation, orderLines)
 
-  private val genCheckProductCodeExist: Gen[CheckProductCodeExist] = for {
-    bool <- Arbitrary.arbBool.arbitrary
-  } yield (_: ProductCode) => bool
-
-  private val genError: Gen[CheckAddressExist] = for {
-    errorMessage <- Gen.alphaNumStr
-  } yield (_: UnvalidatedAddress) => Left(Error(errorMessage))
-
-  private val genLeftAddress: Gen[CheckAddressExist] =
+  private val genCorrectAddress =
     (unvalidatedAddress: UnvalidatedAddress) => Right(Address(unvalidatedAddress.addressLine))
 
-  private val genCheckAddressExist: Gen[CheckAddressExist] = Gen.oneOf(genLeftAddress, genError)
-
   property("validate") {
-    check(forAll(genUnvalidatedOrder, genCheckProductCodeExist, genCheckAddressExist) {
-      (unvalidatedOrder, checkProductCodeExist, checkAddressExist) =>
-        ValidateOrder.validateOrder(checkProductCodeExist, checkAddressExist, unvalidatedOrder) match {
-          case Valid(Order(OrderId(orderId), CustomerInformation(address, emailAddress), orderLines)) => {
-            unvalidatedOrder.orderId == orderId
-          }
-          case _ => false
-        }
+    check(forAll(genUnvalidatedOrder) { unvalidatedOrder =>
+      ValidateOrder.validateOrder(_ => true, genCorrectAddress, unvalidatedOrder) match {
+        case Valid(Order(OrderId(orderId), CustomerInformation(address, emailAddress), orderLines)) =>
+          orderId == unvalidatedOrder.orderId
+        case Invalid(_) =>
+          false
+      }
     })
   }
+
+  def numStrGen(n: Int): Gen[String] = Gen.listOfN(n, Gen.numChar).map(_.mkString)
+  def strGen(n: Int): Gen[String] = Gen.choose(1, n).flatMap(n => Gen.listOfN(n, Gen.numChar).map(_.mkString))
+
 }
